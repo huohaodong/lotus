1eb7c3887cde22c758774ea4977431a354e69531
Huo Haodong <huohaodong.io@gmail.com>
2023-06-13 09:34:39 +0800

    实现 QueryRoutePredicate

--- src/main/java/com/huohaodong/lotus/predicate/QueryRoutePredicate.java
+++ src/main/java/com/huohaodong/lotus/predicate/QueryRoutePredicate.java
@@ -2,10 +2,19 @@ package com.huohaodong.lotus.predicate;
 
 import com.huohaodong.lotus.server.context.GatewayContext;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class QueryRoutePredicate implements RoutePredicate {
-    //TODO: 实现 QueryRoutePredicate
+    Set<String> queries = new HashSet<>();
+
+    public QueryRoutePredicate(PredicateDefinition predicateDefinition) {
+        queries.addAll(List.of(predicateDefinition.getArgs().split(",")));
+    }
+
     @Override
     public boolean test(GatewayContext gatewayContext) {
-        return false;
+        return gatewayContext.getRequest().queryParameters().keySet().containsAll(queries);
     }
 }
